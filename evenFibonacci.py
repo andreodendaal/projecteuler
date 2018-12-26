53fc8e7c0d319badc1c5c3a0779b2d392abeac76
andre odendaal <aodendaal.direct@gmail.com>
2018-12-25 18:43:37 -0800

    fibonacci cleanup

--- evenFibonacci.py
+++ evenFibonacci.py
@@ -1,10 +1,11 @@
 def evenFibonacci(p_target):
     """
-    0 + 1 = 1
-    1 + 1 = 2
+    By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+    find the sum of the even-valued terms.
 
-    :param p_range:
-    :return:
+    >>> evenFibonacci(4000000)
+    4613732
+
     """
     fib_values = []
     fib_values_even = []
@@ -12,17 +13,12 @@ def evenFibonacci(p_target):
     fib_values.append(1)
     i = 0
     while fib_values[i] < p_target:
-    #for i in range(p_range):
-        #print(i)
+
         fib_values.append(fib_values[i+1] + fib_values[i])
         if fib_values[i] % 2 == 0:
             fib_values_even.append(fib_values[i])
 
         i += 1
 
-        #print(fib_values)
-    #print(fib_values)
-    #print(fib_values_even)
     return sum(fib_values_even)
 
-print(evenFibonacci(4000000))
